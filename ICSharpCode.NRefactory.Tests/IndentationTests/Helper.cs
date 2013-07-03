9e7991b88acada37f0815a131deb4acb47b988d8
Matej Miklečić <matej.miklecic@gmail.com>
2013-07-03 21:41:14 +0200

    Added a helper method.
    Creates a new indentation engine and automatically parses the given
    string.

---
+++ ICSharpCode.NRefactory.Tests/IndentationTests/Helper.cs
@@ -0,0 +1,34 @@
+﻿using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.Editor;
+using System.Text;
+
+namespace ICSharpCode.NRefactory.IndentationTests
+{
+    internal static class Helper
+    {
+        public static IndentEngine CreateEngine(string text)
+        {
+            var policy = FormattingOptionsFactory.CreateMono();
+
+            var sb = new StringBuilder();
+            int offset = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                var ch = text[i];
+                if (ch == '$')
+                {
+                    offset = i;
+                    continue;
+                }
+                sb.Append(ch);
+            }
+
+            var document = new ReadOnlyDocument(sb.ToString());
+            var options = new TextEditorOptions();
+
+            var result = new IndentEngine(document, options, policy);
+            result.UpdateToOffset(offset);
+            return result;
+        }
+    }
+}
