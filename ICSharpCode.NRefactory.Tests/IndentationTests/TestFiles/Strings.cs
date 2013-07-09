33efa7726e1ab277d9a6990dce1d7d6d16ab48f0
Matej Miklečić <matej.miklecic@gmail.com>
2013-07-09 22:32:18 +0200

    Implemented all-in-one string tests.

--- ICSharpCode.NRefactory.Tests/IndentationTests/TestFiles/Strings.cs
+++ ICSharpCode.NRefactory.Tests/IndentationTests/TestFiles/Strings.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ICSharpCode.NRefactory.IndentationTests.TestFiles
+﻿namespace Strings
 {
     class Strings
     {
+        void Strings(string s = "")
+        {
+            s = @"""
+cant't escape a verbatim string \";
+            s = @"" "not in verbatim anymore
+            s = "";
+
+            s = "\\";
+            s = "\\\\\\\"";
+            s = " // syntax error, but on the next line we start with the previous state
+            s = "'c\'";
+
+            var c = '\\';
+            c = '\'';
+            c = ' // syntax error, but on the next line we start with the previous state
+            c = ';';
+        }
     }
 }
